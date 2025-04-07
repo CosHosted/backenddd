b50a9374657ac3afee6bfd237f8853f2ca198d58
cowship <38453910+LeQuangNhan-BRVT@users.noreply.github.com>
2025-04-07 16:08:43 +0700

    sua app

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(cors(corsOptions));
 app.all('/', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next()
+  next();
 });
 app.get('/', function (req, res, next) {
   res.send('API đang hoạt động! Sử dụng /api/... để truy cập các endpoints.');
